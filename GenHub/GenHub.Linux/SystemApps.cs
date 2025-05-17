84e6aa175ad7ee18a8b8d4b178f729c490686f3e
kian <lordkian2000@gmail.com>
2025-05-17 18:53:56 +0200

    add props and constructor

--- GenHub/GenHub.Linux/SystemApps.cs
+++ GenHub/GenHub.Linux/SystemApps.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
 namespace GenHub.Linux;
 
 // This class plays the roll of Windows Registry 
@@ -5,7 +10,11 @@ public class SystemApps
 {
     // there should be only 1 instance
     public static SystemApps Instance { get; private set; }
-
+    // list of Apps (binaries) to look for
+    public static readonly string[] AppnNames = ["steam", "flatpak", "lutris"];
+    public List<string> Path { get; private set; } = new List<string>();
+    public List<string> Apps { get; private set; } = new List<string>();
+    public List<string> FlatpackApps { get; private set; } = new List<string>();
     static SystemApps()
     {
         Instance = new SystemApps();
@@ -13,6 +22,19 @@ public class SystemApps
 
     private SystemApps()
     {
-        
+        var path = Environment.GetEnvironmentVariable("PATH");
+        if (path != null)
+            Path.AddRange(path.Split(":").Where(Directory.Exists));
+        foreach (var item in AppnNames)
+        {
+            var res = Whereis(item);
+            if (res.Length > 0)
+                Apps.Add(res);
+        }
+    }
+
+    private string Whereis(string item)
+    {
+        throw new NotImplementedException();
     }
 }

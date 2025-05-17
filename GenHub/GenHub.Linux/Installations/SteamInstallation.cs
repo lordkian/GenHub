67e2bdaec4bce690bf8b9db22fc29e61127b06f8
kian <lordkian2000@gmail.com>
2025-05-17 18:47:20 +0200

    add properties for SteamInstallation (Linux)

--- GenHub/GenHub.Linux/Installations/SteamInstallation.cs
+++ GenHub/GenHub.Linux/Installations/SteamInstallation.cs
@@ -1,6 +1,17 @@
+using GenHub.Core;
+
 namespace GenHub.Linux.Installations;
 
-public class SteamInstallation
+public class SteamInstallation : IGameInstallation
 {
-    
+    // IGameInstallation
+    public GameInstallationType InstallationType => GameInstallationType.Steam;
+    public bool IsVanillaInstalled { get; private set; }
+    public string VanillaGamePath { get; private set; } = "";
+    public bool IsZeroHourInstalled { get; private set; }
+    public string ZeroHourGamePath { get; private set; } = "";
+    public void Fetch()
+    {
+        throw new System.NotImplementedException();
+    }
 }

e9d35cf2e4d992c152fe0fc2aa91edd8b64d1bde
kian <lordkian2000@gmail.com>
2025-05-17 18:51:06 +0200

    add singleton to SystemApps

--- GenHub/GenHub.Linux/SystemApps.cs
+++ GenHub/GenHub.Linux/SystemApps.cs
@@ -1,6 +1,18 @@
 namespace GenHub.Linux;
 
+// This class plays the roll of Windows Registry 
 public class SystemApps
 {
-    
+    // there should be only 1 instance
+    public static SystemApps Instance { get; private set; }
+
+    static SystemApps()
+    {
+        Instance = new SystemApps();
+    }
+
+    private SystemApps()
+    {
+        
+    }
 }

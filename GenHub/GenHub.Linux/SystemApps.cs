9ffde239d67c04a16be45608d8a6eb3374d2c145
kian <lordkian2000@gmail.com>
2025-05-25 15:51:19 +0200

    add HOME to SystemApps

--- GenHub/GenHub.Linux/SystemApps.cs
+++ GenHub/GenHub.Linux/SystemApps.cs
@@ -10,11 +10,14 @@ public class SystemApps
 {
     // there should be only 1 instance
     public static SystemApps Instance { get; private set; }
+
     // list of Apps (binaries) to look for
     public static readonly string[] AppnNames = ["steam", "flatpak", "lutris"];
     public List<string> Path { get; private set; } = new List<string>();
-    public Dictionary<string,string> Apps { get; private set; } = new  Dictionary<string, string>();
+    public Dictionary<string, string> Apps { get; private set; } = new Dictionary<string, string>();
     public List<string> FlatpackApps { get; private set; } = new List<string>();
+    public string HomeAddr { get; private set; }
+
     static SystemApps()
     {
         Instance = new SystemApps();
@@ -23,14 +26,20 @@ public class SystemApps
     private SystemApps()
     {
         var path = Environment.GetEnvironmentVariable("PATH");
-        if (path != null)
-            Path.AddRange(path.Split(":").Where(Directory.Exists));
+        if (string.IsNullOrEmpty(path))
+            throw new Exception("can not access Environment Variables or it's not set: PATH");
+        Path.AddRange(path.Split(":").Where(Directory.Exists));
         foreach (var item in AppnNames)
         {
             var res = Whereis(item);
             if (res.Length > 0)
                 Apps[item] = res;
         }
+
+        HomeAddr = Environment.GetEnvironmentVariable("HOME");
+        if (string.IsNullOrEmpty(HomeAddr))
+            throw new Exception("can not access Environment Variables or it's not set: HOME");
+        // TODO add Exception class for Environment Variables
     }
 
     /// <summary>
@@ -41,8 +50,10 @@ public class SystemApps
     private string Whereis(string program)
     {
         if (Path.Count == 0) return "";
+
         // a query that finds directories containing program
         bool Predict(string item) => File.Exists(System.IO.Path.Combine(item, program));
         // if this query has an output, return it
-        return !Path.Exists(Predict) ? "" : System.IO.Path.Combine(Path.First(Predict), program);    }
+        return !Path.Exists(Predict) ? "" : System.IO.Path.Combine(Path.First(Predict), program);
+    }
 }

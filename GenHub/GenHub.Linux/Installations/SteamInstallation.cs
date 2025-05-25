ea7c5642b71fa226fe73ebdb55cd008bd8787534
kian <lordkian2000@gmail.com>
2025-05-25 16:10:15 +0200

    add DoesSteamPathExist method. for 100% acc we need more testing and finding edge cases

--- GenHub/GenHub.Linux/Installations/SteamInstallation.cs
+++ GenHub/GenHub.Linux/Installations/SteamInstallation.cs
@@ -14,4 +14,23 @@ public class SteamInstallation : IGameInstallation
     {
         throw new System.NotImplementedException();
     }
+
+    /// <summary>
+    /// Checks if steam is installed by looking up the installation path of steam.
+    /// on Linux there must be a reference in .steam directory in HOME dir.
+    /// </summary>
+    /// <returns><c>true</c> if the steam directory was found</returns>
+    private bool DoesSteamPathExist()
+    {
+        if (!systemApps.Apps.ContainsKey("steam") || systemApps.Apps["steam"].Length == 0)
+            return false;
+        if (string.IsNullOrEmpty(systemApps.HomeAddr))
+            return false;
+        if (!Directory.Exists(Path.Combine(systemApps.HomeAddr, ".steam")))
+            return false;
+        // in this case either steam has not been run for the first time of main dir is corrupted
+        return Directory.Exists(Path.Combine(systemApps.HomeAddr, ".steam", "steam")) &&
+               Directory.GetFiles(Path.Combine(systemApps.HomeAddr, ".steam", "steam")).Length != 0;
+        // TODO for 100% acc we need more testing and finding edge cases
+    }
 }

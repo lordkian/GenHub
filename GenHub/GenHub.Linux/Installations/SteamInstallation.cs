a999e1b4f5bdea990c348d845a632d0d139644fa
kian <lordkian2000@gmail.com>
2025-05-25 18:45:58 +0200

    add TryGetSteamLibraries, it is the same as windows version

--- GenHub/GenHub.Linux/Installations/SteamInstallation.cs
+++ GenHub/GenHub.Linux/Installations/SteamInstallation.cs
@@ -32,6 +32,59 @@ public class SteamInstallation : IGameInstallation
     }
 
     /// <summary>
+    /// Tries to fetch all steam library folders containing installed games.
+    /// </summary>
+    /// <param name="steamLibraryPaths">An array of full paths to the common directories for each valid library found.
+    /// This will be null if the method returns <c>false</c>.</param>
+    /// <returns><c>true</c> if at least one steam library path was found.</returns>
+    /// <remarks>
+    /// This method reads the "libraryfolders.vdf" file from the main steam installation directory.
+    /// </remarks>
+    private bool TryGetSteamLibraries(out string[]? steamLibraryPaths)
+    {
+        steamLibraryPaths = null;
+
+        // Try to get the steam path in order to fetch the libraryfolders.vdf
+        if (!TryGetSteamPath(out var steamPath))
+            return false;
+
+        // Find libraryfolders.vdf
+        var libraryFile = Path.Combine(steamPath!, "steamapps", "libraryfolders.vdf");
+
+        if(!File.Exists(libraryFile))
+            return false;
+
+        List<string> results = [];
+
+        // Read all the paths in the vdf and already make them usable.
+        // "C:\\Program Files (x86)\\Steam" to "C:\Program Files (x86)\Steam\steamapps\common"
+        foreach (var line in File.ReadAllLines(libraryFile))
+        {
+            if (!line.Contains("\"path\""))
+                continue;
+
+            var parts = line.Split('"');
+            if(parts.Length < 4)
+                continue;
+
+            var dir = parts[3].Trim();
+
+            if (Directory.Exists(dir))
+            {
+                var path = Path.Combine(dir.Replace(@"\\", @"\"), "steamapps", "common");
+                results.Add(path);
+            }
+        }
+
+        if (results.Count == 0)
+            return false;
+
+        steamLibraryPaths = results.ToArray();
+
+        return true;
+    }
+    
+    /// <summary>
     /// Checks if steam is installed by looking up the installation path of steam.
     /// on Linux there must be a reference in .steam directory in HOME dir.
     /// </summary>
